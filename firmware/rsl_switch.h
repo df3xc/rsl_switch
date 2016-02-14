d247c7474d23d01dbf5cb58e45a30fad1fae1e9f
df3xc <df3xc@web.de>
2016-02-14 04:05:52 +0100

    move functions containing code from library to INO file

--- firmware/rsl_switch.h
+++ firmware/rsl_switch.h
@@ -13,15 +13,22 @@ Copyright : Carsten Lueck, df3xc@web.de
 #ifndef rsl_init_x
 #define rsl_init_x
 
+// Funk Dosen 1..3 sind Deine Dosen in Nord Deutchland
+
 #define DOSE1 1
 #define DOSE2 2
 #define DOSE3 3
+
+// Funk Dosen 4..5 sind meine Dosen in Sued Deutchland
+
+#define DOSE1 4
+#define DOSE2 5
 
 #define EIN 1
 #define AUS 0
 
 extern void conrad_rsl_init();
-extern void conrad_rsl_switch ( int which, int state );
-extern void conrad_rsl_switch_code ( int which, int state );
+extern void conrad_rsl_send(unsigned long code, int SHORT, int LONG);
+extern void conrad_rsl_send(char* code, int SHORT, int LONG);
 
 #endif
