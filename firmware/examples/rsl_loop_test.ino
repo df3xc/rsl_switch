6217a0f7b63893f0f36de2a3ab69d81a81b88adc
Carsten <df3xc@web.de>
2020-08-09 13:01:09 +0200

    new example

---
+++ firmware/examples/rsl_loop_test.ino
@@ -0,0 +1,38 @@
+
+
+
+#include "rsl.h"
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Serial.println(" HELLO ");
+  Serial.println(sizeof(int));
+  Serial.println(sizeof(long));
+  Serial.println(" ----- ");
+  conrad_rsl_init();
+}
+
+void loop() {
+
+  Serial.println("ON");
+  conrad_rsl_switch_code ( 3, 1 );
+  //conrad_rsl_send(2394957312,430,1150);
+  
+  delay(5000);
+  Serial.println("OFF");  
+  conrad_rsl_switch_code ( 3, 0 );  
+  //conrad_rsl_send(2176853504 ,430,1150);  
+  delay(5000); 
+
+//  conrad_rsl_switch_code ( 4, 1 );
+//  delay(5000);
+//  conrad_rsl_switch_code ( 4, 0 );  
+//  delay(5000); 
+//
+//  conrad_rsl_switch_code ( 5, 1 );
+//  delay(5000);
+//  conrad_rsl_switch_code ( 5, 0 );  
+//  delay(5000); 
+
+}
