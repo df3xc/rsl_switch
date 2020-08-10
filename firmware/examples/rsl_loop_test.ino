6f1b72e93d729ec4afb8b92c84d85dbdae9b74a4
Carsten <df3xc@web.de>
2020-08-10 06:49:02 +0200

    update

--- firmware/examples/rsl_loop_test.ino
+++ firmware/examples/rsl_loop_test.ino
@@ -16,23 +16,23 @@ void setup() {
 void loop() {
 
   Serial.println("ON");
-  conrad_rsl_switch_code ( 3, 1 );
+  conrad_rsl_set_switch ( 3, 1 );
   //conrad_rsl_send(2394957312,430,1150);
   
   delay(5000);
   Serial.println("OFF");  
-  conrad_rsl_switch_code ( 3, 0 );  
+  conrad_rsl_set_switch ( 3, 0 );  
   //conrad_rsl_send(2176853504 ,430,1150);  
   delay(5000); 
 
-//  conrad_rsl_switch_code ( 4, 1 );
+//  conrad_rsl_set_switch ( 4, 1 );
 //  delay(5000);
-//  conrad_rsl_switch_code ( 4, 0 );  
+//  conrad_rsl_set_switch ( 4, 0 );  
 //  delay(5000); 
 //
-//  conrad_rsl_switch_code ( 5, 1 );
+//  conrad_rsl_set_switch ( 5, 1 );
 //  delay(5000);
-//  conrad_rsl_switch_code ( 5, 0 );  
+//  conrad_rsl_set_switch( 5, 0 );  
 //  delay(5000); 
 
 }
--- firmware/examples/rsl_test.ino
+++ firmware/examples/rsl_test.ino
@@ -35,51 +35,31 @@ void loop() {
 
       if(ch=='7') {
         Serial.println(" RSL1 ON ");
-        conrad_rsl_switch_code ( 3, 1 );
+        conrad_rsl_set_switch ( 3, 1 );
       }
        if(ch=='8') {
         Serial.println(" RSL1 OFF ");
-        conrad_rsl_switch_code ( 3, 0 );
+        conrad_rsl_set_switch ( 3, 0 );
       }  
 
       if(ch=='4') {
         Serial.println(" RSL2 ON ");
-        conrad_rsl_switch_code ( 4, 1 );
+        conrad_rsl_set_switch ( 4, 1 );
       }
        if(ch=='5') {
         Serial.println(" RSL2 OFF ");
-        conrad_rsl_switch_code ( 4, 0 );
+        conrad_rsl_set_switch ( 4, 0 );
       }  
 
       if(ch=='1') {
         Serial.println(" RSL3 ON ");
-        conrad_rsl_switch_code ( 5, 1 );
+        conrad_rsl_set_switch ( 5, 1 );
       }
        if(ch=='2') {
         Serial.println(" RSL3 OFF ");
-        conrad_rsl_switch_code ( 5, 0 );
+        conrad_rsl_set_switch ( 5, 0 );
       } 
           
     }
-
-//  Serial.println("ON");
-//  conrad_rsl_switch_code ( 3, 1 );
-//  //conrad_rsl_send(2394957312,430,1150);
-//  
-//  delay(5000);
-//  Serial.println("OFF");  
-//  conrad_rsl_switch_code ( 3, 0 );  
-//  //conrad_rsl_send(2176853504 ,430,1150);  
-//  delay(5000); 
-
-//  conrad_rsl_switch_code ( 4, 1 );
-//  delay(5000);
-//  conrad_rsl_switch_code ( 4, 0 );  
-//  delay(5000); 
-//
-//  conrad_rsl_switch_code ( 5, 1 );
-//  delay(5000);
-//  conrad_rsl_switch_code ( 5, 0 );  
-//  delay(5000); 
 
 }
